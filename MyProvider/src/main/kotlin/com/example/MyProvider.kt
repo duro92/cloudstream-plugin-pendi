3ba68643753f6dc1fee6dde5f5078ac3d8201de2
duro92 <114850487+duro92@users.noreply.github.com>
2025-08-10 10:02:33 +0700

    Update MyProvider.kt

--- MyProvider/src/main/kotlin/com/example/MyProvider.kt
+++ MyProvider/src/main/kotlin/com/example/MyProvider.kt
@@ -1,14 +1,17 @@
 package com.example
 
-import android.content.Context
-import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.Plugin
+import com.lagradost.cloudstream3.MainAPI
+import com.lagradost.cloudstream3.TvType
+import com.lagradost.cloudstream3.SearchResponse
 
-@CloudstreamPlugin
-class MyPlugin : Plugin() {
-    override fun load(context: Context) {
-        // daftar semua provider di sini
-        registerMainAPI(MyProvider())
-        registerMainAPI(NgefilmProvider())
+class MyProvider : MainAPI() {
+    override var mainUrl = "https://example.org"
+    override var name = "MyProvider"
+    override val supportedTypes = setOf(TvType.Movie)
+    override val hasMainPage = false
+    override var lang = "id"
+
+    override suspend fun search(query: String): List<SearchResponse> {
+        return emptyList()
     }
 }
