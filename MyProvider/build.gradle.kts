2410a73355f859db6ddd62bdcbe52827c46868a1
duro92 <114850487+duro92@users.noreply.github.com>
2025-08-10 10:00:21 +0700

    Update build.gradle.kts

--- MyProvider/build.gradle.kts
+++ MyProvider/build.gradle.kts
@@ -10,6 +10,11 @@ cloudstream {
     iconUrl = "https://upload.wikimedia.org/wikipedia/commons/4/4e/Pleiades_large.jpg"
 }
 
+dependencies {
+    implementation("org.jsoup:jsoup:1.16.2")
+    implementation("com.github.Blatzar:NiceHttp:0.4.11")
+}
+
 android {
     namespace = "com.example.myprovider"
     buildFeatures {
